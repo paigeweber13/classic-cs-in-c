d053a608aa923b0f0ca6306cdc505629d5f69713
Paige Weber <paigeweber31@gmail.com>
2021-06-06 23:12:16 -0400

    removed short testing stuff

--- sorting-algorithms/src/main.c
+++ sorting-algorithms/src/main.c
@@ -9,33 +9,30 @@
 #define TEST_SIZE 10
 
 int main() {
-  #define SUPPORTEDALGOS_N 1
+  #define SUPPORTEDALGOS_N 6
   const SortAlgorithm supportedAlgos[SUPPORTEDALGOS_N] = 
-    {sortQuickModified};
-  // const size_t maxSize = 11;
-  // const size_t initSize = 10;
-
-  // #define SUPPORTEDALGOS_N 5
-  // const SortAlgorithm supportedAlgos[SUPPORTEDALGOS_N] = 
-  //   {sortInsertion, sortMerge, sortMergeParallel, sortHeap, sortQuick};
+    {sortInsertion, sortMerge, sortMergeParallel, sortHeap, sortQuick, 
+    sortQuickModified};
 
   #define ARRAYSFORTEST_N 3
   const ArrayType arraysForTest[ARRAYSFORTEST_N] = 
     {initRandom, initSorted, initReverse};
 
-  const size_t maxSize = 1000000;
-  // const size_t maxSize = 10000000; // for longer tests
-  const size_t maxSizeInsertion = 16001;
-  // const size_t maxSizeInsertion = 64001; // for longer tests
+  // const size_t maxSize = 1000000;
+  const size_t maxSize = 50000000; // for longer tests
+  // const size_t maxSizeShort = 16001;
+  const size_t maxSizeShort = 64001; // for longer tests
   const size_t initSize = 1000;
 
   printResultHeader();
 
   for (size_t i = 0; i < SUPPORTEDALGOS_N; i++)
     for (size_t j = 0; j < ARRAYSFORTEST_N; j++)
-      if(supportedAlgos[i] == sortInsertion) 
+      if(supportedAlgos[i] == sortInsertion || 
+          supportedAlgos[i] == sortQuick && 
+            (arraysForTest[j] == initSorted || arraysForTest[j] == initReverse)) 
         testSortMulti(supportedAlgos[i], arraysForTest[j], initSize, 
-          maxSizeInsertion);
+          maxSizeShort);
       else
         testSortMulti(supportedAlgos[i], arraysForTest[j], initSize, maxSize);
 
